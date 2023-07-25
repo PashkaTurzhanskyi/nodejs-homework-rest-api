ed3278976dc67deb9f8e75ec990d0a6abe7ea643
PashkaTurzhanskyi <pavlo.turzhanskiy@gmail.com>
2023-07-25 09:02:10 +0300

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,24 +1,27 @@
 const { Schema, model } = require("mongoose");
 const { handleMongooseError } = require("../helpers");
 
-const contactSchema = Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: [true, "Set email for contact"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Set phone for contact"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Set email for contact"],
-  },
-  phone: {
-    type: String,
-    required: [true, "Set phone for contact"],
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: false }
+);
 
 contactSchema.post("save", handleMongooseError);
 
