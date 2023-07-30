a26499a3ebd7b2972adfa9a97577392d61ecc52a
PashkaTurzhanskyi <pavlo.turzhanskiy@gmail.com>
2023-07-30 22:41:11 +0300

    add minimum required

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,6 @@
 const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const isEmptyBody = require("./isEmptyBody");
+const authenticate = require("./authenticate");
 
-module.exports = { validateBody, isValidId, isEmptyBody };
+module.exports = { validateBody, isValidId, isEmptyBody, authenticate };
---
+++ schemas/users.js
@@ -0,0 +1,16 @@
+const Joi = require("joi");
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const registerSchema = Joi.object({
+  password: Joi.string().min(6).required().messages({
+    "any.required": `missed required password field`,
+  }),
+  email: Joi.string().pattern(emailRegexp).required().messages({
+    "any.required": `missed required email field`,
+  }),
+  subscription: Joi.string(),
+  token: Joi.string(),
+});
+
+module.exports = { registerSchema };
