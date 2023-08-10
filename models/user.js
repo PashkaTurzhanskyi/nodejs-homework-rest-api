2f24ed738ea1e058b3f83c4a02208f5ea690ee3e
PashkaTurzhanskyi <pavlo.turzhanskiy@gmail.com>
2023-08-11 01:27:34 +0300

    add hw

--- models/user.js
+++ models/user.js
@@ -26,6 +26,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      default: null,
+    },
   },
   { versionKey: false, timestamps: false }
 );
